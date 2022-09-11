0774e45860ffa47309899ff280123a849442a288
jason rodriguez <rodriguezjason807@DESKTOP-JAY.localdomain>
2022-09-11 00:15:40 -0700

    Updated Waterfall Model

--- Makefile
+++ Makefile
@@ -2,10 +2,9 @@
 all: lab2
 
 lab2: lab2.cpp
-	g++ lab2.cpp -Wall -olab2 -lX11 -lGL -lGLU -lm
-	g++ lab2_old.cpp -Wall -olab2_old -lX11 -lGL -lGLU -lm
-
+	g++ lab2.cpp -Wall -olab2 -lX11 -lGL -lGLU -lm -Wextra libggfonts.a
+	g++ lab2_backup.cpp -Wall -olab2back -lX11 -lGL -lGLU -lm -Wextra libggfonts.a
 clean:
 	rm -f lab2
-	rm -f lab2_old
+	rm -f *.o
 
--- lab2.cpp
+++ lab2.cpp
@@ -1,9 +1,11 @@
-//modified by:Jason Rodriguez
-//date: September 2 2022
+//modified by: Jason Rodriguez
+//date: 8/31/2022
+//
 //
 //author: Gordon Griesel
 //date: Spring 2022
 //purpose: get openGL working on your personal computer
+//
 //
 #include <iostream>
 using namespace std;
@@ -16,41 +18,63 @@ using namespace std;
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
 #include <GL/glx.h>
+#include "fonts.h"
 
-const int MAX_PARTICLES = 10;
-
+//constants
+const int MAX_PARTICLES = 1000;
+const int MAX_BOXES = 5;
 //some structures
+
+
 class Global {
 public:
 	int xres, yres;
 	Global();
 } g;
+class Circle {
+public: 
 
-
+	float radius;
+	float theta;
+	float pi;
+	float pos[2];
+	Circle() {
+	
+		radius = 220.0f;
+		pi = 3.14159f;
+		pos[0] = g.xres * 3.25f;
+		pos[1] = g.yres / 2.75f;
+	}
+}circle;
 class Box {
 public:
-	float w;
+	float w,l;
 	float dir;
 	float vel[2];
 	float pos[2];
 	Box() {
+
 		w = 20.0f;
-		dir = 25.0f;
-		pos[0] = g.xres / 2.0f;
-		pos[1] = g.yres / 2.0f;
+		l = 40.0f;
+		dir = 25.0f; 
+		pos[0] = g.xres / 6.0f;
+		pos[1] = g.yres / 0.5f;
 		vel[0] = vel[1] = 0.0;
+
 	}
 	Box(float wid, float d, float p0, float p1) {
-		w = wid;
-		dir = d;
-		pos[0] = p0;
-		pos[1] = p1;
+	 	w = wid;
+	 	dir = d;
+	 	pos[0] = p0;
+	 	pos[1] = p1;
 		vel[0] = vel[1] = 0.0;
 	}
-} box, particle(2.0, 0.0, g.xres/2.0, g.yres/4.0*3.0);
+} box, particle(2.0,0.0,g.xres/2.0,g.yres/4.0*3.0);
 
 Box particles[MAX_PARTICLES];
+Box boxes[MAX_BOXES];
 int n = 0;
+int m = 0;
 
 class X11_wrapper {
 private:
@@ -103,8 +127,8 @@ int main()
 
 Global::Global()
 {
-	xres = 400;
-	yres = 200;
+	xres = 400; 
+	yres = 200; 
 }
 
 X11_wrapper::~X11_wrapper()
@@ -147,7 +171,7 @@ void X11_wrapper::set_title()
 {
 	//Set the window title bar.
 	XMapWindow(dpy, win);
-	XStoreName(dpy, win, "3350 Lab1");
+	XStoreName(dpy, win, "3350 Lab2");
 }
 
 bool X11_wrapper::getXPending()
@@ -197,19 +221,32 @@ void X11_wrapper::check_resize(XEvent *e)
 
 void make_particle(int x, int y)
 {
-	if (n >= MAX_PARTICLES)
+	if ( n >= MAX_PARTICLES)
 	    return;
-	printf("make_particle(%i, %i)\n", x, y); fflush(stdout);
-	particles[n].w = 2.0;
+    	particles[n].w = 2.0;
 	particles[n].pos[0] = x;
 	particles[n].pos[1] = y;
 	particles[n].vel[0] = particles[n].vel[1] = 0.0;
-	++n;
-	printf("n: %i\n", n); fflush(stdout);
+	n++;
+	
 }
-
-
-
+void make_boxes(int max) {
+	float x = 0.25; 
+	float y = 4;
+	while ( m != max ) {
+		boxes[m].w = 20.0;
+		boxes[m].l = 100.0;
+		boxes[m].pos[0] = g.xres*x; 
+		boxes[m].pos[1] = g.yres*y;
+		boxes[m].vel[0] = boxes[m].vel[1] = 0.0;
+		cout << "box number " << m << ": position (" << boxes[m].pos[0] 
+             << ", " << boxes[m].pos[1] << ") and velocity : " << boxes[m].vel[0] << endl;
+		cout << "repeating " << endl;
+		x = x + 0.50f;
+		y = y - 0.5f;
+		m++;	
+	}
+}
 void X11_wrapper::check_mouse(XEvent *e)
 {
 	static int savex = 0;
@@ -230,9 +267,9 @@ void X11_wrapper::check_mouse(XEvent *e)
 		if (e->xbutton.button==1) {
 			//Left button was pressed.
 			int y = g.yres - e->xbutton.y;
-			int x = e->xbutton.x;
-			make_particle(x,y);	
-			return;
+			int x = e ->xbutton.x;
+			make_particle(x,y);
+		    	return;
 		}
 		if (e->xbutton.button==3) {
 			//Right button was pressed.
@@ -280,66 +317,111 @@ void init_opengl(void)
 	glOrtho(0, g.xres, 0, g.yres, -1, 1);
 	//Set the screen background color
 	glClearColor(0.1, 0.1, 0.1, 1.0);
+	//Allow fonts
+	glEnable(GL_TEXTURE_2D);
+	initialize_fonts();
 }
 
 void physics()
 {
 	particle.vel[1] -= 0.01;
-	particle.pos[0] += particle.vel[0];
+    	particle.pos[0] += particle.vel[0];
 	particle.pos[1] += particle.vel[1];
 	//
-	//check for collision
-	if (particle.pos[1] < (box.pos[1] + box.w) &&
+	// check for collision
+	if (particle.pos[1] < (box.pos[1] + box.w) && 
 	    particle.pos[1] > (box.pos[1] - box.w) &&
 	    particle.pos[0] > (box.pos[0] - box.w) &&
-	    particle.pos[0] < (box.pos[0] + box.w)) {
-		particle.vel[1] = 0.0;
-		particle.vel[0] += 0.01;
+	    particle.pos[0] < (box.pos[0] + box.w)) { 
+	    particle.vel[1] = 0.0;
+	    particle.vel[0] += 0.01;
 	}
-	for (int i=0; i<n; i++) {
-		particles[i].vel[1] -= 0.01;
-		particles[i].pos[0] += particles[i].vel[0];
+	for (int i=0; i<n; i++ ) {
+		particles[i].vel[1] -= 0.75;
+    		particles[i].pos[0] += particles[i].vel[0];
 		particles[i].pos[1] += particles[i].vel[1];
 		//
-		//check for collision
-		if (particles[i].pos[1] < (box.pos[1] + box.w) &&
-		    particles[i].pos[1] > (box.pos[1] - box.w) &&
-		    particles[i].pos[0] > (box.pos[0] - box.w) &&
-		    particles[i].pos[0] < (box.pos[0] + box.w)) {
-			particles[i].vel[1] = 0.0;
-			particles[i].vel[0] += 0.01;
-		}
-		if (particles[i].pos[1] < 0.0) {
-#define OPT_1
-#ifndef OPT_1
-			//This code optimized below
-			particles[i] = particles[n-1];
-			n = n-1;
-#else //OPT_1
-			particles[i] = particles[--n];
-#endif //OPT_1
+		// check for collision
+		for (int j=0; j<MAX_BOXES; j++) {
+			if (particles[i].pos[1] < (boxes[j].pos[1] + boxes[j].w) && 
+	       	    	particles[i].pos[1] > (boxes[j].pos[1] - boxes[j].w) &&
+	     	    	particles[i].pos[0] > (boxes[j].pos[0] - boxes[j].l) &&
+	    	    	particles[i].pos[0] < (boxes[j].pos[0] + boxes[j].l)) { 
+	            	particles[i].vel[1] = 1;
+	            	particles[i].vel[0] += 0.15;
+			}
+			
+			if (particles[i].pos[1] < 0.0 ) {
+		    	particles[i] = particles[--n];
+			}
 		}
 	}
 }
 
 void render()
 {
-	//
+	Rect r;
 	glClear(GL_COLOR_BUFFER_BIT);
-	//Draw box.
+
+	//
+  	//Draw boxes
+	make_boxes(MAX_BOXES);
+	for ( int i = 0; i < MAX_BOXES; i++ ) {	
+		glPushMatrix();
+		glColor3ub(80, 160, 80);
+		glTranslatef(boxes[i].pos[0], boxes[i].pos[1], 0.0f);
+		glBegin(GL_QUADS);
+			glVertex2f(-boxes[i].l, -boxes[i].w);
+			glVertex2f(-boxes[i].l,  boxes[i].w);
+			glVertex2f( boxes[i].l,  boxes[i].w);
+			glVertex2f( boxes[i].l, -boxes[i].w);
+		glEnd();
+		glPopMatrix();
+	}
+	//
+	//Print Words
+	r.bot = g.yres - 20;
+	r.left = 10;
+	r.center = 0;
+	ggprint8b(&r, 20, 0x00ff0000, " Waterfall Model ");
+	ggprint8b(&r, 20, 0x00ff0000, " GO FULLSCREEN ");
+	r.bot = g.yres * 0.80;
+	r.left = 50;
+	r.center = 0;
+	ggprint8b(&r, 150, 0x00ffff00, " REQUIREMENTS ");
+	r.bot = g.yres * 0.70;
+	r.left = 270;
+	r.center = 0;
+	ggprint8b(&r, 16, 0x00ffff00, " DESIGN ");
+	r.bot = g.yres * 0.60;
+	r.left = 470;
+	r.center = 0;
+	ggprint8b(&r, 16, 0x00ffff00, "CODING ");
+	r.bot = g.yres * 0.50;
+	r.left = 670;
+	r.center = 0;
+	ggprint8b(&r, 16, 0x00ffff00, " TESTING ");
+	r.bot = g.yres * 0.40;
+	r.left = 860;
+	r.center = 0;
+	ggprint8b(&r, 16, 0x00ffff00, " MAINTENANCE ");
+	//
+	//Draw Circle
 	glPushMatrix();
-	glColor3ub(150, 200, 120);
-	glTranslatef(box.pos[0], box.pos[1], 0.0f);
-	glBegin(GL_QUADS);
-		glVertex2f(-box.w, -box.w);
-		glVertex2f(-box.w,  box.w);
-		glVertex2f( box.w,  box.w);
-		glVertex2f( box.w, -box.w);
+	glColor3ub(80, 160, 80);
+	glBegin(GL_POLYGON);
+	for (int i=0; i<360; i++ ) {
+		circle.theta = i*circle.pi/180;
+		glVertex2f(circle.pos[0] + circle.radius * cos(circle.theta),
+                   circle.pos[1] + circle.radius * sin(circle.theta));	
+	}	
 	glEnd();
 	glPopMatrix();
+	
+	//
 	//Draw particle.
 	glPushMatrix();
-	glColor3ub(150, 160, 255);
+	glColor3ub(50,60, 255);
 	glTranslatef(particle.pos[0], particle.pos[1], 0.0f);
 	glBegin(GL_QUADS);
 		glVertex2f(-particle.w, -particle.w);
@@ -348,10 +430,11 @@ void render()
 		glVertex2f( particle.w, -particle.w);
 	glEnd();
 	glPopMatrix();
-	//Draw all particles.
-	for (int i=0; i<n; i++) {
+	
+	//Draw particles.
+	for (int i=0; i<n ; i++ ) {
 		glPushMatrix();
-		glColor3ub(150, 160, 255);
+		glColor3ub(150,160, 255);
 		glTranslatef(particles[i].pos[0], particles[i].pos[1], 0.0f);
 		glBegin(GL_QUADS);
 			glVertex2f(-particles[i].w, -particles[i].w);
